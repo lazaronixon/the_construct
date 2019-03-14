053be8c33109a04065daff6405b0c11a3a255525
Lázaro Nixon <lazaronixon@hotmail.com>
2019-03-14 08:45:38 -0300

    add turbolinks_form_controller

---
+++ app/javascript/controllers/turbolinks_form_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+  connect() {
+    this.element.addEventListener("ajax:beforeSend", function(event) {
+      const detail = event.detail, xhr = detail[0], options = detail[1]
+
+      Turbolinks.visit(options.url, { action: "replace" })
+      event.preventDefault()
+    })
+  }
+}
