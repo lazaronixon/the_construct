c0999a96c8d4cb8a4b43db7e3e38b7b6659ca3c9
Lázaro Nixon <lazaronixon@hotmail.com>
2019-09-14 03:31:15 -0300

    cosmetic

--- test/support/test_capybara_helper.rb
+++ test/support/test_capybara_helper.rb
@@ -1,16 +1,16 @@
 module TestCapybaraHelper
-  def select2(value = nil, from: nil, **options)
+  def select2(value = nil, from: nil)
     find(:label, text: from).click
     find('.select2-container--focus').click
     find('.select2-results__option', text: value, match: :first).click
   end
 
-  def check_label(locator = nil, **options)
+  def check_label(locator = nil)
     checkbox = find(:checkbox, locator, visible: false)
     checkbox.ancestor(:label).click unless checkbox.checked?
   end
 
-  def uncheck_label(locator = nil, **options)
+  def uncheck_label(locator = nil)
     checkbox = find(:checkbox, locator, visible: false)
     checkbox.ancestor(:label).click if checkbox.checked?
   end
