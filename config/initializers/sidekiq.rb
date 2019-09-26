80b249c89f5b41660dcc87afc9dfb9706473da6c
Lázaro Nixon <lazaronixon@hotmail.com>
2019-09-26 09:45:23 -0300

    fix sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.options = config.options.merge({ concurrency: ENV.fetch('WORKER_MAX_THREADS') { 5 }, retry: false })
+  config.options = config.options.merge({ queues: %w[ default active_storage_analysis ], concurrency: ENV.fetch("WORKER_MAX_THREADS") { 5 } })
+  config.default_worker_options = config.default_worker_options.merge({ retry: false })  
 end if defined?(Sidekiq)
+
+Sidekiq.configure_client do |config|
+  config.default_worker_options = config.default_worker_options.merge({ retry: false })
+end if defined?(Sidekiq)
+
