8aa0524c113118c72676ebe0cda8f932750aa91d
Lázaro Nixon <lazaronixon@hotmail.com>
2019-12-29 23:45:27 -0300

    remove default_url_options

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,10 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Initialize default_url_options
-Rails.application.routes.default_url_options[:host] = Rails.configuration.app['url']
-
 # Initialize SMTP Settings
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => Rails.configuration.app['url'],
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
